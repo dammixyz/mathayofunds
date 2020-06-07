7170233f42904fe805c28f3beb7978b5117382b4
dammieight <dammieight@gmail.com>
2020-06-07 02:47:17 +0100

    login register

---
+++ resources/views/includes/footer-bar.blade.php
@@ -0,0 +1,6 @@
+<div class="footer-bar">
+    <div class="container">
+        <span>All right reserved. Mathayofunds © 2020</span>
+        <span><img src="{{asset('mathayofunds.png')}}" /></span>
+    </div>
+</div>
