53f6714764470e78852622c581b5f38df3365de0
dammieight <dammieight@gmail.com>
2020-06-22 14:13:05 +0100

    withdrawal .. withdrawal-history

--- resources/views/pages/withdrawals-history.blade.php
+++ resources/views/pages/withdrawals-history.blade.php
@@ -25,6 +25,9 @@
     {{--File upload--}}
     <link href="assets/css/scrollspyNav.css" rel="stylesheet" type="text/css" />
     <link href="file-upload/file-upload-with-preview.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css"/>
+
     <script>
         (function (i, s, o, g, r, a, m) {
             i['GoogleAnalyticsObject'] = r;
@@ -139,57 +142,219 @@
                 </div>
             </div>
             <div class="col-md-9 col-md-offset-0 col-sm-8 col-sm-offset-2 res-m-bttm">
-                <div class="box round shadow-alt text-center">
-                    <form action="#" method="post">
-                        <div class="row">
-                            <div class="col-md-8 col-md-offset-2">
-                                <h5 style="font-family: OpenSans; font-weight: bold">Withdrawal</h5>
-                                <div class="clearfix">
-                                    <div id="withdrawal">
-                                        <div class="form-field form-m-bttm">
-                                            <label for="withdrawal_amount" class="text-sm">How much would you like to withdraw?</label>
-                                            <input name="withdrawal_amount" id="withdrawal_amount"
-                                                   class="form-control required" aria-required="true"
-                                                   placeholder="Enter Amount in naira"
-                                                   type="number" required>
+                <div class="box round shadow-alt mb-15">
+                    <div class="d-flex justify-content-between">
+                        <h6 class="ucap">Withdrawal History</h6>
+                    </div>
+                    <p class="small">History of all your withdrawals</p>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="sidebar-right wgs-box">
+                                <div class="wgs-search">
+                                    <div class="wgs-content">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control"
+                                                   id="search-withdrawals-history"
+                                                   placeholder="Search Withdrawals History...">
+                                            <button class="search-btn"><i class="fa fa-search"
+                                                                          aria-hidden="true"></i>
+                                            </button>
                                         </div>
                                     </div>
-                                </div>
-                                <hr>
-                                <h5 style="font-family: OpenSans; font-weight: bold">Your Current Bank Details</h5>
-                                <div class="clearfix">
-                                    <div id="bank_details">
-                                        <div class="form-field form-m-bttm">
-                                            <label for="bank" class="text-sm">Bank Name</label>
-                                            <input name="bank" value="Guarantee Trust Bank"
-                                                   class="form-control required" aria-required="true"
-                                                   type="text" disabled required>
-                                        </div>
-                                        <div class="form-field form-m-bttm">
-                                            <label for="account_no" class="text-sm">Bank Account
-                                                Number</label>
-                                            <input name="account_no" value="0283929193"
-                                                   class="form-control required" aria-required="true"
-                                                   type="text" disabled required>
-                                        </div>
-                                        <div class="form-field form-m-bttm">
-                                            <label for="full_name" class="text-sm">Full Name (As per
-                                                bank)</label>
-                                            <input name="full_name" value="Stunner Stunner"
-                                                   class="form-control required" aria-required="true"
-                                                   type="text" disabled required>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-field form-button form-m-bttm">
-                                    <button type="submit" class="btn btn-xs btn-alt btn-color"
-                                            style="margin-top: 20px; padding-top: 10px; padding-bottom: 10px">
-                                        Withdraw
-                                    </button>
+                                    <div class="gaps size-1x"></div>
                                 </div>
                             </div>
                         </div>
-                    </form>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table table-hover mb-15" id="withdrawalsHistory">
+                            <thead>
+                            <tr>
+                                <th scope="col">S/N</th>
+                                <th scope="col">Amount</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>&#8358; 50, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orange">Pending
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>&#8358; 30, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: green">Completed
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>&#8358; 33, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orangered">
+                                        Canceled
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>&#8358; 50, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orange">Pending
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>&#8358; 30, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: green">Completed
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>&#8358; 33, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orangered">
+                                        Canceled
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>&#8358; 50, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orange">Pending
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>&#8358; 30, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: green">Completed
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>&#8358; 33, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orangered">
+                                        Canceled
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>&#8358; 50, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orange">Pending
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>&#8358; 30, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: green">Completed
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>&#8358; 33, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orangered">
+                                        Canceled
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>&#8358; 50, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orange">Pending
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>&#8358; 30, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: green">Completed
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>&#8358; 33, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orangered">
+                                        Canceled
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>&#8358; 50, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orange">Pending
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>&#8358; 30, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: green">Completed
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>&#8358; 33, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orangered">
+                                        Canceled
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>&#8358; 50, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orange">Pending
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>&#8358; 30, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: green">Completed
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>&#8358; 33, 000</td>
+                                <td>
+                                    <div class="badge" style="background-color: orangered">
+                                        Canceled
+                                    </div>
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -339,39 +504,35 @@
     })
 </script>
 
-<!-- Slimscroll -->
-<script src="{{asset('assets_dashboard/jquery.slimscroll.min.js')}}"></script>
-
-
 <script src="assets/js/scrollspyNav.js"></script>
 <script src="file-upload/file-upload-with-preview.min.js"></script>
-<script>
-    var firstUpload = new FileUploadWithPreview('profilePicture')
-    $(function(){
-        $('#latest-transaction-table').slimScroll({
-            height: '250px',
-            allowPageScroll: false,
-            size: '1px',
-            color: '#f7921a',
-            opacity: 0.5,
-        });
-        $('#coins-rates-table').slimScroll({
-            height: '250px',
-            allowPageScroll: false,
-            size: '1px',
-            color: '#f7921a',
-            opacity: 0.5
-        });
-        $('#giftcards-rates-table').slimScroll({
-            height: '250px',
-            allowPageScroll: false,
-            size: '1px',
-            color: '#f7921a',
-            opacity: 0.5
-        });
-    });
-</script>
 
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+
+
+<script>
+    $(window).on('load', function () {
+        $('div#themes_panel').hide();
+
+        /*Data Table*/
+        const cardTable = $('#withdrawalsHistory').DataTable({
+            responsive: true,
+            bPaginate: true,
+            bLengthChange: false,
+            bFilter: true,
+            bInfo: true,
+            bAutoWidth: true,
+            pageLength: 15,
+        });
+        /*Remove the default search box*/
+        $('#withdrawalsHistory_filter').remove()
+        /*Add datatable search functionality to custom search box*/
+        $('#search-withdrawals-history').keyup(function () {
+            cardTable.search($(this).val()).draw();
+        })
+    })
+</script>
 </body>
 
 </html>
