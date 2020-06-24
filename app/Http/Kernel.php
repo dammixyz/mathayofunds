78a467e5ec49800f7b4a29b2fd9c79c000e0c45c
unknown <emmanueldmlr@gmail.com>
2020-06-24 11:53:34 +0100

    Worked on admin dashboard

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'checkAuth' => \App\Http\Middleware\checkAuth::class,
+        'checkAdmin' => \App\Http\Middleware\checkAdmin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
